39dbf0bc599c1a4d90a6deee54bf99af85446fbf
Kalson <Kalson.Kalu@yahoo.com>
2014-05-21 11:28:41 -0500

    Create InboxViewController.h

---
+++ InboxViewController.h
@@ -0,0 +1,14 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by KaL on 5/13/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+- (IBAction)logout:(id)sender;
+
+@end
